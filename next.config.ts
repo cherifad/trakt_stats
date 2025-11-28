358f071407b548601dfc86601e6889f0719dedd8
Adlen CHERIF <101065369+cherifad@users.noreply.github.com>
2025-11-28 15:52:35 +0000

    chore(deps): add framer-motion, next-themes and update dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.fanart.tv',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'secure.gravatar.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'walter-r2.trakt.tv',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
